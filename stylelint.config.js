9e45634213d6f1178fe70717e34eaf023f3e71af
JusticHentai <1360636180@qq.com>
2022-12-01 19:09:26 +0800

    feat: 添加button

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     '@justichentai/stylelint-config/basic',
     '@justichentai/stylelint-config/scss',
-    '@justichentai/stylelint-config/vue',
   ],
   rules: {
     // mask 不带前缀会有问题
